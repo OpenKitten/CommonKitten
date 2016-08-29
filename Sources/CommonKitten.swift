de7d5c0c2429ab8eb6322cbb52af011471c45861
Joannis Orlandos <j.orlandos@jakajima.eu>
2016-08-29 08:13:41 +0200

    Added Strings

--- Sources/CommonKitten.swift
+++ Sources/CommonKitten.swift
@@ -4,6 +4,24 @@ public enum InstantiationError : Error {
     case notEnoughData
 }
 
+/// All errors that can occur when (de)serializing
+public enum DeserializationError : Error {
+    /// The instantiating went wrong because the element has an invalid size
+    case InvalidElementSize
+    
+    /// The contents of the BSON binary data was invalid
+    case InvalidElementContents
+    
+    /// The lsat element of the Binary Array was invalid
+    case InvalidLastElement
+    
+    /// Something went wrong with parsing (yeah.. very specific)
+    case ParseError
+    
+    /// This operation was invalid
+    case InvalidOperation
+}
+
 extension Integer {
     /// The amount of bytes in one of `Self`
     public static var size: Int {
@@ -27,5 +45,91 @@ extension Integer {
         }
         
         return UnsafePointer<Self>(data).pointee
+    }
+}
+
+public extension String {
+    /// This `String` as c-string
+    public var cStringBytes : [UInt8] {
+        var byteArray = self.utf8.filter{$0 != 0x00}
+        byteArray.append(0x00)
+        
+        return byteArray
+    }
+    
+    /// Instantiate a string from BSON (UTF8) data, including the length of the string.
+    public static func instantiate(bytes data: [UInt8]) throws -> String {
+        var 🖕 = 0
+        
+        return try instantiate(bytes: data, consumedBytes: &🖕)
+    }
+    
+    /// Instantiate a string from BSON (UTF8) data, including the length of the string.
+    public static func instantiate(bytes data: [UInt8], consumedBytes: inout Int) throws -> String {
+        let res = try _instant(bytes: data)
+        consumedBytes = res.0
+        return res.1
+    }
+    
+    private static func _instant(bytes data: [UInt8]) throws -> (Int, String) {
+        // Check for null-termination and at least 5 bytes (length spec + terminator)
+        guard data.count >= 5 && data.last == 0x00 else {
+            throw DeserializationError.InvalidLastElement
+        }
+        
+        // Get the length
+        let length = try Int32.instantiate(bytes: Array(data[0...3]))
+        
+        // Check if the data is at least the right size
+        guard data.count >= Int(length) + 4 else {
+            throw DeserializationError.ParseError
+        }
+        
+        // Empty string
+        if length == 1 {
+            return (5, "")
+        }
+        
+        guard length > 0 else {
+            throw DeserializationError.ParseError
+        }
+        
+        var stringData = Array(data[4..<Int(length + 3)])
+        
+        guard let string = String(bytesNoCopy: &stringData, length: stringData.count, encoding: String.Encoding.utf8, freeWhenDone: false) else {
+            throw DeserializationError.ParseError
+        }
+        
+        return (Int(length + 4), string)
+    }
+    
+    /// Instantiate a String from a CString (a null terminated string of UTF8 characters, not containing null)
+    public static func instantiateFromCString(bytes data: [UInt8]) throws -> String {
+        var 🖕 = 0
+        
+        return try instantiateFromCString(bytes: data, consumedBytes: &🖕)
+    }
+    
+    /// Instantiate a String from a CString (a null terminated string of UTF8 characters, not containing null)
+    public static func instantiateFromCString(bytes data: [UInt8], consumedBytes: inout Int) throws -> String {
+        let res = try _cInstant(bytes: data)
+        consumedBytes = res.0
+        return res.1
+    }
+    
+    private static func _cInstant(bytes data: [UInt8]) throws -> (Int, String) {
+        guard data.contains(0x00) else {
+            throw DeserializationError.ParseError
+        }
+        
+        guard let stringData = data.split(separator: 0x00, maxSplits: 1, omittingEmptySubsequences: false).first else {
+            throw DeserializationError.ParseError
+        }
+        
+        guard let string = String(bytes: stringData, encoding: String.Encoding.utf8) else {
+            throw DeserializationError.ParseError
+        }
+        
+        return (stringData.count+1, string)
     }
 }
